9641620ec2742dc4d9ee8c61fc9c53207f93bf1f
maeganwilson <maegan@maeganwilson.com>
2021-05-03 07:41:37 -0500

    Initial Commit

---
+++ pokemon-tutorial-api/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokemon-tutorial-api
+//
+//  Created by Maegan Wilson on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokemon-tutorial-api/pokemon_tutorial_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_tutorial_apiApp.swift
+//  pokemon-tutorial-api
+//
+//  Created by Maegan Wilson on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_tutorial_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
